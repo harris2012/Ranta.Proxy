5f1aec474e070694d4e157d706a46c182b1f3adc
HarrisZhang <harriszhang@live.cn>
2016-06-19 10:25:35 +0800

    update AssemblyInfo

--- Ranat.Proxy/Properties/AssemblyInfo.cs
+++ Ranat.Proxy/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Ranta.Proxy.Net45")]
+[assembly: AssemblyTitle("Ranta.Proxy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Ranta.Proxy.Net45")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("Ranta")]
+[assembly: AssemblyProduct("Ranta.Proxy")]
+[assembly: AssemblyCopyright("Copyright © Ranta 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("103c2533-b24d-4236-9647-4ff231dea6f2")]
+[assembly: Guid("6da63982-fae5-49da-989d-34d1c6b1c309")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
